ee49828af55ecaf8c218f383f17c71e2d0fe109b
Viktor <Cezn.Vi@gmail.com>
2022-11-15 20:05:25 +0200

    start

---
+++ C-Sharp_Money/C-Sharp_Money/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C_Sharp_Money
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
