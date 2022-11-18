3c16f2ba3b5ba8f65237dd14876b4097f2fe3617
Viktor <Cezn.Vi@gmail.com>
2022-11-18 20:18:22 +0200

    WIP

--- C-Sharp_Money/Program.cs
+++ C-Sharp_Money/Program.cs
@@ -13,15 +13,23 @@ namespace C_Sharp_Money
             Console.OutputEncoding = Encoding.Unicode;
 
 
-            Money m = new Money(0.98);
-            Money m1 = new Money(1.13);
-
-            m += m1;
-            Console.WriteLine(m);
-            m += 0.55;
-            Console.WriteLine(m);
-
+            Money m = new (1.90);
+            Money m1 = new (1.9);
+            Money m2 = new(int.MaxValue);
             
+
+            Console.WriteLine($"M = {m}\nM1= {m1}");
+
+
+
+            Console.WriteLine($"Тест виключення  \"переповнення змінної\" при + : {m} + {double.MaxValue} = {m = m + int.MaxValue}");
+
+
+            //Console.WriteLine($"Тест виключення  ділення на 0 {m=m/0}");
+            //Console.WriteLine($"Тест виключення банкрот {m} - 500 = {m = m - 500}");
+            //m = new(12, 22);
+            //Console.WriteLine($"Тест виключення  переповнення змінної при множенні: {m} * {int.MaxValue} = {m = m * int.MaxValue}");
+
         }
     }
 }
